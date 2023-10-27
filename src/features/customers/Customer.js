c2e8640f33460fcfff5bc57d6017b1c023b37b55
Skerdi Shehaj <skerdianshehaj@gmail.com>
2023-10-28 01:56:25 +0200

    Dipatched respective actions on products and accounts functions

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,7 +1,8 @@
 import { useSelector } from 'react-redux';
 
 function Customer() {
-  const customer = useSelector((store) => store.customer.fullname);
+  // ! Whenever the store changes the component will re-render
+  const customer = useSelector((store) => store.customer.fullName);
   return <h2>👋 Welcome, {customer}</h2>;
 }
 
