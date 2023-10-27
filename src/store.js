d47f9bd7bb1b1a413e836ec9712a9d7dcadd210a
Skerdi Shehaj <skerdianshehaj@gmail.com>
2023-10-28 00:03:36 +0200

    Added action creator functions for redux

---
+++ src/store.js
@@ -0,0 +1,94 @@
+import { createStore } from 'redux';
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: '',
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'account/deposit':
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case 'account/withdraw':
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    case 'account/requestLoan':
+      if (state.loan > 0) return state;
+      // ! later
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case 'account/payLoan':
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: '',
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+// store.dispatch({ type: 'account/deposit', payload: 1000 });
+// console.log(store.getState());
+
+// store.dispatch({ type: 'account/withdraw', payload: 100 });
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: 'account/requestLoan',
+//   payload: { purpose: 'For fun', amount: 1000 },
+// });
+// console.log(store.getState());
+
+// store.dispatch({ type: 'account/payLoan' });
+// console.log(store.getState());
+
+function deposti(amount) {
+  return {
+    type: 'account/deposit',
+    payload: amount,
+  };
+}
+function withdraw(amount) {
+  return {
+    type: 'account/withdraw',
+    payload: amount,
+  };
+}
+function requestLoan(purpose, amount) {
+  return {
+    type: 'account/requestLoan',
+    payload: { purpose: purpose, amount: amount },
+  };
+}
+function payLoan() {
+  return {
+    type: 'account/payLoan',
+  };
+}
+
+store.dispatch(deposti(1000));
+console.log(store.getState());
+
+store.dispatch(withdraw(100));
+console.log(store.getState());
+
+store.dispatch(requestLoan('For fun', 1000));
+console.log(store.getState());
+
+store.dispatch(payLoan());
+console.log(store.getState());
